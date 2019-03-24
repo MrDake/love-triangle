b75808be836cefb63b8cad10a736b73636218bbf
MrDake <nalulxd@gmail.com>
2019-03-24 14:59:33 +0600

    feat: add code to index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,20 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+
+  let count=0;
+    
+ for(let i = 0; i < preferences.length; i++) {
+    let b = preferences[i]-1;
+    let c = preferences[b]-1;
+    let a = preferences[c]-1;
+    if (i == a) {
+        count=count+1;
+    }
+        
+ };
+ 
+ count = Math.floor(count/3);
+ return count;
+ };
+
